70b8a3073cfeb4b88b090686cc8133e26b5292c9
Nabin Kandel <105159506+nabin-kandel@users.noreply.github.com>
2023-01-23 19:06:24 +0545

    Update form.js

--- form.js
+++ form.js
@@ -1,11 +1,38 @@
+
 function validateForm()
 {
-
-
+    var pas= document.forms["myForm"]["password"].value;
     let x= document.forms["myForm"]["username"].value;
+    var W= pas.length;
+
     if (x == "") {
       alert("Username must be filled out");
       return false;
     }
 
+    if (pas == "") {
+      alert("Password must be filled out");
+      return false;
+    }
+
+else{
+  
+if(W>6){
+
+  var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,28}$/;
+     if (re.test(pas))
+       {
+       
+       return true;
+  }
+  else{
+    alert('Your password must satisfy the following. \n\n* Password should be 6 to 28 character long. \n* Password should have at least one alphabet. \n* Password should have at least one numeric value. \n* Password should  have special characters.');
+    return false;
+  }
+}
+
+else{
+  alert("Password length must be Greater then 6");
+  return false;
+}}
 }
